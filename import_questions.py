48a03cd1b273e23587c75d57e63e17bf8bf8cddb
IG <goyalishita705@gmail.com>
2026-01-23 00:31:00 +0530

    questioncsvfile

---
+++ import_questions.py
@@ -0,0 +1,19 @@
+import csv
+from student.models import Question
+
+with open("question_bank_real_20_per_class.csv", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        Question.objects.create(
+            class_level=int(row["class"]),
+            subject_name=row["subject_name"],
+            difficulty=row["difficulty"],
+            question_text=row["question_text"],
+            option_a=row["option_a"],
+            option_b=row["option_b"],
+            option_c=row["option_c"],
+            option_d=row["option_d"],
+            correct_option=row["correct_option"],
+        )
+
+print("Questions imported successfully")
--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Question, AssessmentResult
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("class_level", "subject_name", "difficulty")
+    list_filter = ("class_level", "subject_name", "difficulty")
+    search_fields = ("question_text",)
+
+@admin.register(AssessmentResult)
+class AssessmentResultAdmin(admin.ModelAdmin):
+    list_display = (
+        "student",
+        "subject_name",
+        "easy_correct",
+        "medium_correct",
+        "hard_correct",
+        "created_at",
+    )
+    list_filter = ("subject_name", "created_at")
